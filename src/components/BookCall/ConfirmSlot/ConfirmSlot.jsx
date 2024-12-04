1e157ec1f15e9658f34ee93fda769877105cfdd9
Krishna <cu.17bcs1796@gmail.com>
2024-12-04 20:17:19 +0530

    :fire: Id 11 completed.

--- src/components/BookCall/ConfirmSlot/ConfirmSlot.jsx
+++ src/components/BookCall/ConfirmSlot/ConfirmSlot.jsx
@@ -4,22 +4,22 @@ import PrimaryButton from '@/components/Buttons/PrimaryButton/PrimaryButton';
 import SecondaryButton from '@/components/Buttons/SecondaryButton/SecondaryButton';
 import { IoLogoWhatsapp } from "react-icons/io";
 
-function ConfirmSlot({onClose}) {
+function ConfirmSlot({ onClose }) {
     return (
         <>
             <div className={styles.headerBox} >
-                <h3><span></span>Don't leave just yet! Let us assist you. 
-                <span><RxCross1 onClick={onClose} className={styles.cancelbtn} /></span>
+                <h3><span></span>Don't leave just yet! Let us assist you.
+                    <span><RxCross1 onClick={onClose} className={styles.cancelbtn} /></span>
                 </h3>
                 <p>Need some support? We're just a click away.</p>
             </div>
             <div className={styles.progressBox}>
-                <div className="">
+                <div>
                     <h3>1</h3>
                     <p>Select date and time</p>
                 </div>
                 <div className={styles.progressLine} ></div>
-                <div className="">
+                <div>
                     <h3>2</h3>
                     <p>Confirm  your slot</p>
                 </div>
@@ -30,7 +30,7 @@ function ConfirmSlot({onClose}) {
                 <PrimaryButton>Confirme</PrimaryButton>
             </div>
             <div className={styles.whatsAppBox}>
-            <IoLogoWhatsapp  className={styles.whatsAppLogo}/>
+                <IoLogoWhatsapp className={styles.whatsAppLogo} />
                 <div className={styles.whatsAppText}>
                     <h3>Need more assistance? Reach out to us on WhatsApp.</h3>
                     <p>WhatsApp us now</p>
