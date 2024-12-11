5b3d12c2f35c3711a63ab82e33f3d1731df5246e
priyanb <priyanb250@gmail.com>
2024-12-11 12:46:33 +0530

    Complaints Table Completed

--- src/components/SEBIDisclosure/ComplaintsTable/ComplaintsTable.jsx
+++ src/components/SEBIDisclosure/ComplaintsTable/ComplaintsTable.jsx
@@ -1,7 +1,30 @@
-"use client"
+import { useState, useEffect } from 'react';
 import styles from './ComplaintsTable.module.css';
 
-const ComplaintsTable = ({content}) => {
+const ComplaintsTable = ({ content }) => {
+
+  const [rows, setRows] = useState(content.data || []);
+  const [isClient, setIsClient] = useState(false);
+
+  // Ensuring client-side rendering to Prevent Hydration error
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    setRows(content.data || []);
+  }, [content]);
+
+  const handleStatusChange = (index, newStatus) => {
+    const updatedRows = [...rows];
+    updatedRows[index].Compliance_Audit_Status = newStatus;
+    setRows(updatedRows);
+  };
+
+  if (!isClient) {
+    return <div>Loading...</div>; 
+  }
+ 
   return (
     <div className={styles.tableContainer}>
       <table className={styles.table}>
@@ -15,13 +38,20 @@ const ComplaintsTable = ({content}) => {
           </tr>
         </thead>
         <tbody>
-          {content.data.map((row, rowIndex) => (
+          {rows.map((row, rowIndex) => (
             <tr key={rowIndex} className={styles.tr}>
               {content.columns.map((column, colIndex) => (
                 <td key={colIndex} className={styles.td}>
-
-                  {row[column.label] != null ? row[column.label] : '-'}
-
+                  {column.label === 'Compliance_Audit_Status' ? (
+                    <input
+                      type="text"
+                      value={row.Compliance_Audit_Status || ""}
+                      onChange={(e) => handleStatusChange(rowIndex, e.target.value)}
+                      className={styles.tableInput}
+                    />
+                  ) : (
+                    row[column.label] != null ? row[column.label] : '-'
+                  )}
                 </td>
               ))}
             </tr>
