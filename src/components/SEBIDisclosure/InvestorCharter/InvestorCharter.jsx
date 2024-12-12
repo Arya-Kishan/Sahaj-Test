4de4b0579f985be87b53700014e6d29819e345e6
Krishna <cu.17bcs1796@gmail.com>
2024-12-12 18:17:12 +0530

    minor changes

--- src/components/SEBIDisclosure/InvestorCharter/InvestorCharter.jsx
+++ src/components/SEBIDisclosure/InvestorCharter/InvestorCharter.jsx
@@ -9,7 +9,7 @@ const InvestorCharter = () => {
           <p>SEBI's Investor Charter</p>
         </div>
         <section className={style.pdfContainer} >
-        <PdfViewer pdfUrl={"https://pdfobject.com/pdf/sample.pdf"}/>
+        {/* <PdfViewer pdfUrl={"https://pdfobject.com/pdf/sample.pdf"}/> */}
       </section>
       
     </>
--- src/components/SEBIDisclosure/PdfViewer.jsx
+++ src/components/SEBIDisclosure/PdfViewer.jsx
@@ -10,11 +10,11 @@ const PdfViewer = ({ pdfUrl }) => {
 
   return (
     <div style={{height: "500px", overflow: "auto", border: "1px solid #ddd" }}>
-      {/* <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
           <Viewer 
           fileUrl={pdfUrl} 
           plugins={[defaultLayoutPluginInstance]} />
-      </Worker> */}
+      </Worker>
     </div>
 
   );
