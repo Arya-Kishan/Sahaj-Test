c172a67c3aef003148caf896e813db339b523963
Krishna <cu.17bcs1796@gmail.com>
2024-12-13 18:55:48 +0530

    npm install pdfjs-dist

--- src/components/SEBIDisclosure/InvestorCharter/InvestorCharter.jsx
+++ src/components/SEBIDisclosure/InvestorCharter/InvestorCharter.jsx
@@ -1,6 +1,6 @@
 "use client"
 import style from "./investorCharter.module.css"
-import PdfViewer from "../PdfViewer";
+// import PdfViewer from "../PdfViewer";
 
 const InvestorCharter = () => {
   return (
@@ -9,7 +9,7 @@ const InvestorCharter = () => {
           <p>SEBI's Investor Charter</p>
         </div>
         <section className={style.pdfContainer} >
-        <PdfViewer pdfUrl={"https://pdfobject.com/pdf/sample.pdf"}/>
+        {/* <PdfViewer pdfUrl={"https://pdfobject.com/pdf/sample.pdf"}/> */}
       </section>
       
     </>
