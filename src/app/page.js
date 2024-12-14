50bf4e8426af1bfeedba71d0338c28c4798c865f
Krishna <cu.17bcs1796@gmail.com>
2024-12-14 17:59:45 +0530

    minor chnages

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import FinanceQuestions from "@/components/Home/FinanceQuestions/FinanceQuestion
 import FinancialPlan from "@/components/Home/FinancialPlan/FinancialPlan";
 import WhyChooseUs from "@/components/Home/WhyChooseUs/WhyChooseUs";
 import Testimonials from "@/components/Home/Testimonials/Testimonials";
-const apiUrl = process.env.BASE_URL;
 export default function Home() {
   return (
     <>
@@ -14,7 +13,6 @@ export default function Home() {
     <Carousel />
       <FeaturedIn />
       <FinanceQuestions/>
-      <h1>{apiUrl}</h1>
       <WhyChooseUs/>
       <FinancialPlan/>
       <Testimonials/>   
