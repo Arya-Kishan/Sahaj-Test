a798a862e8d58371ae2d53e6c2dd4c5afb8330d2
Krishna <cu.17bcs1796@gmail.com>
2024-12-25 19:01:30 +0530

    :fire: download finance plan form integrated.

--- src/components/DownloadModal/Download.jsx
+++ src/components/DownloadModal/Download.jsx
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./Modal.module.css";
+import Image from "next/image";
 import { IoIosArrowDown } from "react-icons/io";
 import { countryCode } from "../BookCall/countrycode";
+import { downloadData } from "@/services/download";
+import checkLogo from "../../../public/logos/check.png" 
 
 const DownloadModal = ({ isOpen, onClose }) => {
     const [formData, setFormData] = useState({
@@ -11,27 +14,42 @@ const DownloadModal = ({ isOpen, onClose }) => {
     });
     const [countryData, setCountryData] = useState("91");
     const [codeDropdown, setOpenClose] = useState(false);
+    const [responseTrue, setResponse ] = useState(false)
 
     const isFormValid = formData.fullName && formData.email && formData.phone;
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-
-        if (name === "phone") {
-            setFormData({ ...formData, [name]: `+${countryData}${value}` });
-        } else {
-            setFormData({ ...formData, [name]: value });
-        }
+        setFormData({ ...formData, [name]: value });
     };
 
     const handleCountryCodeChange = (code) => {
         setCountryData(code);
-
         setOpenClose(false);
     };
-    const handleSubmit = (e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("Form Data Submitted:", formData);
+
+        const formattedData = {
+            full_name: formData.fullName,
+            email: formData.email,
+            country_code: `+${countryData}`,
+            phone_number: formData.phone,
+        };
+
+        console.log("Formatted Data Submitted:", formattedData);
+
+        try {
+            const { res, err } = await downloadData(formattedData);
+            if (res) {
+                console.log(res);
+                setResponse(true)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
         setFormData({
             fullName: "",
             email: "",
@@ -44,9 +62,20 @@ const DownloadModal = ({ isOpen, onClose }) => {
     return (
         <div className={styles.modalOverlay}>
             <div className={styles.modalContent}>
-                <button className={styles.closeButton} onClick={onClose}>
+            <button className={styles.closeButton} onClick={onClose}>
                     &times;
                 </button>
+                {responseTrue ?
+                <>
+                <div className={styles.successBox}>
+                <div className={styles.box1}>
+                    <div className={styles.box2}><Image  src={checkLogo} alt="succes logo" srcset="" /></div>
+                </div>
+                <h3>Your free financial plan has been sent to your email address.</h3>
+                </div>
+                </>: 
+                <>
+                
                 <h2 className={styles.formHeader}>
                     Fill out the form to get your free plan sent directly to your inbox.
                 </h2>
@@ -94,10 +123,11 @@ const DownloadModal = ({ isOpen, onClose }) => {
                                 className={styles.Pinput}
                                 type="tel"
                                 name="phone"
-                                value={formData.phone.replace(`+${countryData}`, "")}
+                                value={formData.phone}
                                 onChange={handleInputChange}
                                 placeholder="Enter your phone number"
                                 required
+                                max={10}
                             />
                             {codeDropdown && (
                                 <div className={styles.dropBox}>
@@ -105,9 +135,7 @@ const DownloadModal = ({ isOpen, onClose }) => {
                                         countryCode.map((ele) => (
                                             <p
                                                 key={ele.code}
-                                                onClick={() =>
-                                                    handleCountryCodeChange(ele.code)
-                                                }
+                                                onClick={() => handleCountryCodeChange(ele.code)}
                                             >
                                                 + {ele.code} {ele.name}
                                             </p>
@@ -118,13 +146,15 @@ const DownloadModal = ({ isOpen, onClose }) => {
                     </label>
                     <button
                         type="submit"
-                        className={`${styles.submitBTN} ${isFormValid ? styles.submitActive : styles.submitDisabled
-                            }`}
+                        className={`${styles.submitBTN} ${
+                            isFormValid ? styles.submitActive : styles.submitDisabled
+                        }`}
                         disabled={!isFormValid}
                     >
                         Submit
                     </button>
                 </form>
+                </>}
             </div>
         </div>
     );
