6d26da918d0db8ae94defa100e8419ff476deca8
priyanb <priyanb250@gmail.com>
2024-12-29 20:45:42 +0530

    resolved  brower errors

--- src/components/AboutUs/CompanyInfo/CompanyInfo.jsx
+++ src/components/AboutUs/CompanyInfo/CompanyInfo.jsx
@@ -30,33 +30,34 @@ const SectionOne = () => {
     return (
         <div className={styles.sectionOneContainer}>
             {Data &&
-                Data?.map((item, index) => {
-                    return <>
-                        <div key={index} className={styles.informationtextContainer}>
-                            <div className={styles.infotext}>
-                                <p className={styles.infotextheading}>{item?.Title}</p>
-                                <p className={styles.infotextbody}>{item?.Content[index]?.SmallMainDescription}</p>
-                                <Link href="/about/investmentphilosophy" ><ReadMore text={"Read More"} /></Link>
-                                
-                            </div>
-
-                            <div className={styles.infoimgBox}>
-                                <iframe
-                                    width={300}
-                                    height={300}
-                                    className={styles.infoimg}
-                                    src={item?.VideoLink}
-                                    title="YouTube video player"
-                                    frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                    allowFullScreen>
-                                </iframe>
-
-                                {/* <Image src={companyInfoImg} className={styles.infoimg} alt="CompanyInfoImage" /> */}
-                            </div>
+                Data?.map((item, index) => 
+                   (
+                    <div key={index} className={styles.informationtextContainer}>
+                        <div className={styles.infotext}>
+                            <p className={styles.infotextheading}>{item?.Title}</p>
+                            <p className={styles.infotextbody}>{item?.Content[index]?.SmallMainDescription}</p>
+                            <Link href="/about/investmentphilosophy" ><ReadMore text={"Read More"} /></Link>
+                            
                         </div>
-                    </>
-                })
+
+                        <div className={styles.infoimgBox}>
+                            <iframe
+                                width={300}
+                                height={300}
+                                className={styles.infoimg}
+                                src={item?.VideoLink}
+                                title="YouTube video player"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowFullScreen>
+                            </iframe>
+
+                            {/* <Image src={companyInfoImg} className={styles.infoimg} alt="CompanyInfoImage" /> */}
+                        </div>
+                    </div>
+              
+                   )
+                )
 
             }
         </div>
