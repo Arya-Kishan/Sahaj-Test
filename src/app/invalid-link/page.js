a231c2312169d1e02305770b5a674b8a6cb1c034
Krishna <cu.17bcs1796@gmail.com>
2024-12-31 13:31:53 +0530

    minor change

--- src/app/invalid-link/page.js
+++ src/app/invalid-link/page.js
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function InvalidLinkPage() {
     return (
       <div style={{ textAlign: "center", marginTop: "50px" }}>
         <h1>Invalid Link</h1>
         <p>The requested video link is not available. Please try again later.</p>
-        <a href="/" style={{ color: "blue", textDecoration: "underline" }}>
+        <Link href="/" style={{ color: "blue", textDecoration: "underline" }}>
           Go back to the home page
-        </a>
+        </Link>
       </div>
     );
   }
