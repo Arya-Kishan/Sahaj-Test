5fcedcead4c622f20f0fd8caab80b2298311ebb0
priyanb <priyanb250@gmail.com>
2025-01-05 17:31:55 +0530

    Sebi Table Column header Values According to figma Fixed

--- src/components/SEBIDisclosure/ComplaintsTable/ComplaintsTable.jsx
+++ src/components/SEBIDisclosure/ComplaintsTable/ComplaintsTable.jsx
@@ -2,54 +2,21 @@
 
 import styles from './ComplaintsTable.module.css';
 
-const ComplaintsTable = ({ tableData = {}, totals, tableType,tableHeaderData=[] }) => {
-  // Define the column mappings for each table type
-  const columnMappings = {
-    lastMonth: {
-      "Received From": "ReceivedFrom",
-      "Pending as of last month": "PendingLastMonth",
-      "Received": "Received",
-      "Resolved*": "Resolved",
-      "Total Pending#": "TotalPending",
-      "Pending >3 months": "Pending3Month",
-      "Avg. Resolution Time^": "AverageResolutionTime"
-    },
-    monthlyTrend: {
-      "Month": "month",
-      "Carried forward from previous month": "Carried_forward_from_previous_month",
-      "Received": "Received",
-      "Resolved*": "Resolved",
-      "Total Pending#": "Pending"
-    },
-    annualTrend: {
-      "Year": "year",
-      "Carried forward from previous month": "carriedForward",
-      "Received": "received",
-      "Resolved": "resolved",
-      "Total Pending#": "pending"
-    },
-    annualCompliance: {
-      "Financial Year": "year",
-      "Compliance Status": "status",
-      "Remarks, if any": "remark"
-    }
-  };
+const ComplaintsTable = ({ tableData = {}, totals,tableHeaderData=[] }) => {
 
-  // Get the columns for the selected table type
   const columns = tableData.length > 0 ? Object.keys(tableData[0]) : [];
   const totalRow = totals ? Object.keys(totals).map((key) => totals[key]) : [];
-  const currentMapping = columnMappings[tableType] || {};
-console.log("the haaa",tableHeaderData)
+
   return (
     <div className={styles.tableContainer}>
       <table className={styles.table}>
         <thead>
           <tr className={styles.tr}>
-            <th className={styles.th}><strong>SR No</strong></th>
+            <th className={styles.th}><strong>Sr No.</strong></th>
             {tableHeaderData.map((column) => (
               column !== '_id' && (
                 <th key={column} className={styles.th}>
-                  <strong>{column}</strong> 
+                  <strong>{column||"Col Name"}</strong> 
                 </th>
               )
             ))}
