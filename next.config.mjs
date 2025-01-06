8c1897cc75089ccf5738755ae56f80ea7df93319
Arya <arya69kishan@gmail.com>
2025-01-06 08:15:47 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,24 +9,12 @@ const nextConfig = {
     return config;
   },
   images: {
-    // For testing, adding (img.freepik.com) and (www.business-standard.com) domains. Remove later.
-    domains: [
-      'sahajmoney-bucket.s3.ap-south-1.amazonaws.com',
-      'sahajmoney.com',
-      'img.freepik.com',
-      'www.business-standard.com',
-    ],
-  },
+    //for tetsing adding (img.freepik.com)(www.business-standard.com) should be removed later
+    domains: ['sahajmoney-bucket.s3.ap-south-1.amazonaws.com','sahajmoney.com','img.freepik.com',"www.business-standard.com"]
+},
+
   env: {
     NEXT_APP_BASE_URL: process.env.NEXT_APP_BASE_URL,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://insecure-api.com/:path*',
-      },
-    ];
   },
 };
 
