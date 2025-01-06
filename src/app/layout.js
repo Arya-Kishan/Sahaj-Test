ec2c127653c394da54498d376280400a26d6186f
Arya <arya69kishan@gmail.com>
2025-01-06 07:57:49 +0530

    removed headers

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,11 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </head>
       <body>
         <ReduxProvider>
           <Navbar />
